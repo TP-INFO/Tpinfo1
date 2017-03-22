89960d72e4db616f8487db19ce6294301816dfa8
david di Marcantonio <david@code4mac.com>
2017-03-22 13:56:07 +0100

    ajout de TODO

--- Tpinfo1/Program.cs
+++ Tpinfo1/Program.cs
@@ -8,9 +8,22 @@ namespace Tpinfo1
 {
     class Program
     {
+        /// <summary>
+        /// Définit la valeur de ma version actuelle
+        /// </summary>
+        const string version = "0.1";
+
         static void Main(string[] args)
         {
+            //TODO : tester si le ou les arguments existent
+            //TODO : En fonction de l'argument passé en mode console 
+            // - appeler une fonction interne
+            // OU 
+            // - instancier un nouvel objet
+           
 
         }
+
+
     }
 }
