00b3aeba5a7961fcf97d3093acde33a80c96e8a5
MarouenKanoun <marouen1931@hotmail.com>
2017-03-23 12:53:33 +0100

    premier commit

---
+++ TP 1 a poucher/salairie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication173
+{
+    class Salarie
+    {/// <summary>
+     ///  declarer les attribut 
+     /// </summary>
+        public int matricule;
+        public int categorie;
+        public int service;
+        public string nom;
+        public double salaire;
+
+        public Salarie(string lenom, double lesalire)
+        {
+            nom = lenom;
+            salaire = lesalire;
+        }
+        public String  Calculesalire()
+        {
+            return $"le salaire de {nom } et de {salaire}"; 
+        }
+
+    }
+}
