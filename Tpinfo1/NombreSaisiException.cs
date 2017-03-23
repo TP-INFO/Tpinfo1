d2285240c2f34784196088cded24fcf78a779c2d
HerveRimbaud <rvrimbaud34@gmail.com>
2017-03-23 15:39:27 +0100

    Push 1 random + exception

---
+++ Tpinfo1/NombreSaisiException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tpinfo1
+{
+    class NombreSaisiException : Exception
+    {
+        public NombreSaisiException()
+        {
+        } 
+
+        
+        public override string ToString()
+        {
+            return "On t'a dit de rentrer un nombre entre 0 et 30 !";
+        }
+    }
+}
--- Tpinfo1/Program.cs
+++ Tpinfo1/Program.cs
@@ -9,21 +9,54 @@ namespace Tpinfo1
     class Program
     {
         /// <summary>
-        /// Définit la valeur de ma version actuelle
+        /// Définit la valeur de ma version actuelle.
         /// </summary>
         const string version = "0.1";
 
         static void Main(string[] args)
         {
-            //TODO : tester si le ou les arguments existent
-            //TODO : En fonction de l'argument passé en mode console 
-            // - appeler une fonction interne
-            // OU 
-            // - instancier un nouvel objet
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args.Length);
 
-           
+            }
+
+            //l'ordinateur génère un nombre aléatoire.
+
+            try
+            {
+                Console.WriteLine("Générer un nombre aléatoire par l'ordinateur");
+                trouverUnNombre();
+            }
+            catch (NombreSaisiException nse)
+            {
+                Console.WriteLine(nse);
+            }
 
         }
+        public static void trouverUnNombre()
+        {
+            Random nbre = new Random();
+            int nbreAlea = nbre.Next(30);
+            int nbreSaisi = -1;
+            int count = 0;
+            while (nbreSaisi != nbreAlea)
+            {
+                Console.WriteLine("Saisissez un nombre compris en 0 et 30");
+                nbreSaisi = Convert.ToInt32(Console.ReadLine());
+                if (nbreSaisi < 0 || nbreSaisi > 30)
+                {
+                    NombreSaisiException nse = new NombreSaisiException();
+                    throw nse;
+                }
+                count++;
+
+            }
+            Console.WriteLine("Tu as gagné en {0} coups. Veux-tu essayer une fois de plus ?", count);
+
+        }
+
+    }
 
 
     }
